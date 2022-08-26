1b7b0017a3c62f155e81acdfffa7caeaf04f2386
Oscar Diaz <oscar.diazpt@gmail.com>
2022-08-26 17:23:12 -0500

    Html basico a javascript

---
+++ javascript.js
@@ -0,0 +1,14 @@
+var num1=1;
+var num2=2;
+var num3=3;
+var num4=4;
+var num5=5;
+var num6=6;
+var num7=7;
+var num8=8;
+var num9=9;
+var num9=9;
+var num10=10;
+var num11=11;
+var num12=12;
+document.getElementById
